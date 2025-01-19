d8e45eeb82047ab601c94e305160b1c01156dc11
Ihliana <Ihnatco@protonmail.com>
2025-01-19 16:58:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Guess-the-Letter
+# Guess the Letter 🎯 
+
+A simple word game where players attempt to guess a randomly selected letter of the alphabet. Feedback is provided for each guess to guide the player closer to the correct answer.
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NUM_GUESSES = 5;
+const NUM_ATTEMPTS = 5;
+
+/**
+ * Guess the letter app
+ * @returns {JSX.Element}
+ */
+
+function App() {
+ 
+
+  return (
+    <div className="App min-h-screen bg-gradient-to-br from-green-500 to-green-800 flex flex-col items-center justify-center text-white font-sans">
+     
+  
+    
+
+     
+    
+    </div>
+  );
+}
+
+export default App;
