39584e779eef1a74dea9feb454f83f45d9b640be
Ihliana <Ihnatco@protonmail.com>
2025-01-21 15:09:46 +0100

    GameFeedback component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Button({children}){
-    return <button>{children}</button>
+    return <button className={`px-6 py-3`}>{children}</button>
 }
 
 
---
+++ src/components/Game/GameFeedback.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function GameFeedback({solution}){
+    return <h2 className='font-semibold m-8'>{solution}</h2>
+}
+
+export default GameFeedback;
