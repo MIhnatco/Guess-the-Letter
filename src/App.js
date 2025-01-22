0b58aff3a226611f05379911c05cd82dccfd3ce3
Ihliana <Ihnatco@protonmail.com>
2025-01-22 16:36:17 +0100

    Game management and reset game

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import GameHeader from "./components/Game/GameHeader";
 import GameInput from "./components/Game/GameInput";
 import GameBoard from "./components/Game/GameBoard";
 import GameFeedback from "./components/Game/GameFeedback";
-import GameReset from "./components/Game/GameReset"
+import GameReset from "./components/Game/GameReset";
 
 /**
  * Guess the letter app
@@ -16,6 +16,9 @@ function App() {
     <div className="App min-h-screen bg-gradient-to-br from-[#85A98F] to-[#5A6C57] md:text-2xl flex flex-col items-center justify-center text-white font-sans">
       <GameHeader />
       <GameInput />
+      <GameBoard />
+      <GameFeedback />
+      <GameReset />
     </div>
   );
 }
