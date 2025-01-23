aa6c524d465d515ca6cc60dc2c47ddfe03e4e94f
Ihliana <Ihnatco@protonmail.com>
2025-01-23 14:14:06 +0100

    Confetti animation if win

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Confetti from "react-confetti";
+
 import GameHeader from "./components/Game/GameHeader";
 import GameInput from "./components/Game/GameInput";
 import GameBoard from "./components/Game/GameBoard";
@@ -14,13 +16,25 @@ import useGameLogic from "./components/hooks/useGameLogic";
  */
 
 function App() {
-
-  const {letter,guesses, attempts, solution, setSolution, playerGuess, handlePlayerChange, handleGuess, isWinner, resetGame} = useGameLogic();
+  const {
+    letter,
+    guesses,
+    attempts,
+    solution,
+    setSolution,
+    playerGuess,
+    handlePlayerChange,
+    handleGuess,
+    isWinner,
+    resetGame,
+  } = useGameLogic();
   return (
     <div className="App min-h-screen bg-gradient-to-br from-[#85A98F] to-[#5A6C57] md:text-2xl flex flex-col items-center justify-center text-white font-sans">
+      {isWinner && <Confetti />}
+
       <GameHeader />
 
-      <GameInput 
+      <GameInput
         playerGuess={playerGuess}
         handlePlayerChange={handlePlayerChange}
         handleGuess={handleGuess}
@@ -28,7 +42,6 @@ function App() {
         letter={letter}
         setSolution={setSolution}
       />
-
 
       <GameBoard guesses={guesses} attempts={attempts} />
       <GameFeedback solution={solution} />
